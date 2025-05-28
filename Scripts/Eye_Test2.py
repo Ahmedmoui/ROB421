81a6894dcef7bf14b7a66e0c7a2a28889dbe5261
Ahmedmoui <ahmedamoussaoui@gmail.com>
2025-05-28 13:19:58 -0700

    Changed to fullscreen

--- Scripts/Eye_Test2.py
+++ Scripts/Eye_Test2.py
@@ -7,7 +7,7 @@ os.environ['SDL_VIDEO_WINDOW_POS'] = "0,0"
 
 # Initialize
 pygame.init()
-screen = pygame.display.set_mode((480, 800), pygame.NOFRAME)
+screen = pygame.display.set_mode((480, 800), pygame.FULLSCREEN)
 pygame.mouse.set_visible(False)
 
 pygame.display.set_caption("Blinking Eyes - Portrait")
